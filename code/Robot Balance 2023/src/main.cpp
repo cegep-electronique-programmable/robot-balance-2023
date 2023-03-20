02f830287a211144ab693f0e320e5ca672868f4a
Julien Boriasse <julien@boriasse.com>
2023-03-20 14:57:11 -0400

    Initialisation UART et GPIO dans des fonctions

--- code/Robot Balance 2023/src/main.cpp
+++ code/Robot Balance 2023/src/main.cpp
@@ -1,19 +1,72 @@
 #include <Arduino.h>
+#include "board_mapping.h"
 
-#define PIN_TEST 16
+// Fonction d'initialisation des GPIO
+int initialisationGPIO(void) {
+
+  // Boutons utilisateurs (entrées seulement)
+  pinMode(GPIO_B1, INPUT);
+  pinMode(GPIO_B2, INPUT);
+
+  // Points de test (entrées seulement)
+  pinMode(GPIO_TP1, INPUT);
+  pinMode(GPIO_TP1, INPUT);
+
+  // Desactivation des moteurs au démarrage
+  pinMode(GPIO_ENABLE_MOTEURS, INPUT);
+  digitalWrite(GPIO_ENABLE_MOTEURS, LOW);
+
+  // Moteur droit
+  pinMode(GPIO_DIR_D, OUTPUT);
+  pinMode(GPIO_STEP_D, OUTPUT);
+  pinMode(GPIO_MS1_D, OUTPUT);
+  pinMode(GPIO_MS2_D, OUTPUT);
+  pinMode(GPIO_MS3_D, OUTPUT);
+  digitalWrite(GPIO_DIR_D, LOW);
+  digitalWrite(GPIO_MS1_D, LOW);
+  digitalWrite(GPIO_MS2_D, LOW);
+  digitalWrite(GPIO_MS3_D, LOW);
+
+  // Moteur gauche
+  pinMode(GPIO_DIR_G, OUTPUT);
+  pinMode(GPIO_STEP_G, OUTPUT);
+  pinMode(GPIO_MS1_G, OUTPUT);
+  pinMode(GPIO_MS2_G, OUTPUT);
+  pinMode(GPIO_MS3_G, OUTPUT);
+  digitalWrite(GPIO_DIR_G, LOW);
+  digitalWrite(GPIO_MS1_G, LOW);
+  digitalWrite(GPIO_MS2_G, LOW);
+  digitalWrite(GPIO_MS3_G, LOW);
+
+  // LED
+  pinMode(GPIO_DATA_LED, OUTPUT);
+
+  // SPI
+  pinMode(GPIO_VPSI_CS1, OUTPUT);
+  pinMode(GPIO_VPSI_SCK, OUTPUT);
+  pinMode(GPIO_VPSI_MISO, INPUT);
+  pinMode(GPIO_VPSI_MOSI, OUTPUT);
+  digitalWrite(GPIO_VPSI_CS1, HIGH);
+
+  // SPI
+  pinMode(GPIO_I2C_SDA, OUTPUT);
+  pinMode(GPIO_I2C_SCL, OUTPUT);
+
+  return 0;
+}
+
+// Fonction d'initialisation de l'UART
+int initialisationUART(void) {
+  Serial.begin(115200);
+  printf("Robot Balance 2023\r\n");
+  return 0;
+}
 
 void setup() {
-  Serial.begin(115200);
-  Serial.println("Hello World!");
-
-  pinMode(PIN_TEST, OUTPUT);
+  initialisationUART();
+  initialisationGPIO();
 }
 
 void loop() {
-  Serial.println("bonjour");
-  printf("TX: %d, RX: %d\r\n", TX, RX);
-  digitalWrite(PIN_TEST, HIGH);
-  delay(1000);
-  digitalWrite(PIN_TEST, LOW);
-  delay(1000);
+  printf(".\r\n");
 }
