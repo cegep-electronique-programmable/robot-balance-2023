d3aa768bcdc5285eec6d99d99401292b69c7634f
Julien Boriasse <julien@boriasse.com>
2023-03-20 14:20:16 -0400

    GPIO Pin 24 Output GPIO2 MS1_G

--- code/Robot Balance 2023/src/main.cpp
+++ code/Robot Balance 2023/src/main.cpp
@@ -1,12 +1,17 @@
 #include <Arduino.h>
 
+#define PIN_TEST 2
 void setup() {
   Serial.begin(115200);
   Serial.println("Hello World!");
+
+  pinMode(PIN_TEST, OUTPUT);
 }
 
 void loop() {
   Serial.println("bonjour");
-
-  delay(200);
+  digitalWrite(PIN_TEST, HIGH);
+  delay(1000);
+  digitalWrite(PIN_TEST, LOW);
+  delay(1000);
 }
