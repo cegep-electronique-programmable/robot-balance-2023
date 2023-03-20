cb3634f654dddb3590b97628bc92574a77485d45
Julien Boriasse <julien@boriasse.com>
2023-03-20 14:10:02 -0400

    uart ok

--- code/Robot Balance 2023/src/main.cpp
+++ code/Robot Balance 2023/src/main.cpp
@@ -1,9 +1,12 @@
 #include <Arduino.h>
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println("Hello World!");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  Serial.println("bonjour");
+
+  delay(200);
 }
