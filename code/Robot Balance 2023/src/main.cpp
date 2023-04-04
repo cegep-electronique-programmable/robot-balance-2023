c639affb42c8a6a3166f5cd8bf6c8d0a0f6e4565
Julien Boriasse <julien@boriasse.com>
2023-04-04 12:39:57 -0400

    PoC operationnelle. Besoin d'un cleanup

--- code/Robot Balance 2023/src/main.cpp
+++ code/Robot Balance 2023/src/main.cpp
@@ -9,7 +9,21 @@
 #include <Adafruit_NeoPixel.h>
 
 
-#define WIFI_ACTIVE ENTERPRISE
+float target_speed_degrees_per_second_moteur_1 = 90;
+int ratio_moteur_1 = 4;
+int step_per_tour_moteur_1 = 200;
+int delai_timer_moteur_1 = 1000000;
+
+
+float target_speed_degrees_per_second_moteur_2 = 45;
+int ratio_moteur_2 = 4;
+int step_per_tour_moteur_2 = 200;
+int delai_timer_moteur_2 = 1000000;
+
+
+#define KP 20
+
+//#define WIFI_ACTIVE ENTERPRISE
 
 #include "secrets.h"
 
@@ -23,15 +37,167 @@ const char *ssid = WIFI_SSID;
 const char *password = WIFI_PASSWORD;
 #else
 #include "esp_wpa2.h"
-const char *ssid = EAP_SSID; 
+const char *ssid = EAP_SSID;
 int counter = 0;
 #endif
 #endif
 
-int timeout = 0;
+hw_timer_t *Timer0_Cfg = NULL;
+hw_timer_t *Timer3_Cfg = NULL;
+
+
+
+int angle = 0;
+int angle_sp = 8;
+int erreur = 0;
+int output = 0;
+
+
+void IRAM_ATTR Timer0_ISR() {
+
+  if (target_speed_degrees_per_second_moteur_1 > 0) {
+    digitalWrite(GPIO_DIR_G, LOW);
+  } else {
+    digitalWrite(GPIO_DIR_G, HIGH);
+  }
+
+  digitalWrite(GPIO_STEP_G, HIGH);
+  delayMicroseconds(2);
+  digitalWrite(GPIO_STEP_G, LOW);
+
+  float step_per_second = abs(target_speed_degrees_per_second_moteur_1) / 360.0 * step_per_tour_moteur_1 * ratio_moteur_1;
+  delai_timer_moteur_1 = 1000000 / step_per_second;
+
+  timerAlarmWrite(Timer0_Cfg, delai_timer_moteur_1, true);
+  timerAlarmEnable(Timer0_Cfg);
+}
+
+void IRAM_ATTR Timer3_ISR() {
+
+  if (target_speed_degrees_per_second_moteur_2 > 0) {
+    digitalWrite(GPIO_DIR_D, HIGH);
+  } else {
+    digitalWrite(GPIO_DIR_D, LOW);
+  }
+
+  digitalWrite(GPIO_STEP_D, HIGH);
+  delayMicroseconds(2);
+  digitalWrite(GPIO_STEP_D, LOW);
+
+  float step_per_second = abs(target_speed_degrees_per_second_moteur_2) / 360.0 * step_per_tour_moteur_2 * ratio_moteur_2;
+  delai_timer_moteur_2 = 1000000 / step_per_second;
+
+  timerAlarmWrite(Timer3_Cfg, delai_timer_moteur_2, true);
+  timerAlarmEnable(Timer3_Cfg);
+}
+
+
+
+// Accéléromètre
+MXC6655 accel;
+
+// Instanciation des dels
+Adafruit_NeoPixel pixels(NEOPIXEL_COUNT, NEOPIXEL_PIN, NEO_GRB + NEO_KHZ800);
+int pixel = 0;
+
+int timeoutVAR = 0;
+
+
+
+
+// Fonction d'initialisation des GPIO
+int initialisationGPIO(void)
+{
+
+  // Boutons utilisateurs (entrées seulement)
+  pinMode(GPIO_B1, INPUT);
+  pinMode(GPIO_B2, INPUT);
+
+  // Points de test (entrées seulement)
+  pinMode(GPIO_TP1, INPUT);
+  pinMode(GPIO_TP1, INPUT);
+
+  // Desactivation des moteurs au démarrage
+  pinMode(GPIO_ENABLE_MOTEURS, INPUT);
+  digitalWrite(GPIO_ENABLE_MOTEURS, LOW);
+
+  // Moteur droit
+  pinMode(GPIO_DIR_D, OUTPUT);
+  pinMode(GPIO_STEP_D, OUTPUT);
+  pinMode(GPIO_MS1_D, OUTPUT);
+  pinMode(GPIO_MS2_D, OUTPUT);
+  pinMode(GPIO_MS3_D, OUTPUT);
+  digitalWrite(GPIO_DIR_D, LOW);
+  digitalWrite(GPIO_MS1_D, LOW);
+  digitalWrite(GPIO_MS2_D, LOW);
+  digitalWrite(GPIO_MS3_D, LOW);
+
+  // Moteur gauche
+  pinMode(GPIO_DIR_G, OUTPUT);
+  pinMode(GPIO_STEP_G, OUTPUT);
+  pinMode(GPIO_MS1_G, OUTPUT);
+  pinMode(GPIO_MS2_G, OUTPUT);
+  pinMode(GPIO_MS3_G, OUTPUT);
+  digitalWrite(GPIO_DIR_G, LOW);
+  digitalWrite(GPIO_MS1_G, LOW);
+  digitalWrite(GPIO_MS2_G, LOW);
+  digitalWrite(GPIO_MS3_G, LOW);
+
+  // LED
+  pinMode(GPIO_DATA_LED, OUTPUT);
+
+  // SPI
+  pinMode(GPIO_VPSI_CS1, OUTPUT);
+  pinMode(GPIO_VPSI_SCK, OUTPUT);
+  pinMode(GPIO_VPSI_MISO, INPUT);
+  pinMode(GPIO_VPSI_MOSI, OUTPUT);
+  digitalWrite(GPIO_VPSI_CS1, HIGH);
+
+  // SPI
+  pinMode(GPIO_I2C_SDA, OUTPUT);
+  pinMode(GPIO_I2C_SCL, OUTPUT);
+
+  return 0;
+}
+
+// Fonction d'initialisation de l'UART
+int initialisationUART(void)
+{
+  Serial.setPins(GPIO_UART_TX, GPIO_UART_RX);
+  Serial.begin(115200);
+  printf("Robot Balance 2023\r\n");
+  return 0;
+}
+
+int initialisationsNeoPixel(void)
+{
+  pixels.begin();
+  pixels.clear();
+  pixels.setBrightness(0x22);
+  // Toutes les DEL en rouge pendant l'initialisation
+  for (int i = 0; i < NEOPIXEL_COUNT; i++)
+  {
+    pixels.setPixelColor(i, pixels.Color(0x00, 0x00, 0x00));
+  }
+  pixels.show();
+  return 0;
+}
+
+
+
 
 void setup()
 {
+  int initilisation_reussie = 0;
+  initilisation_reussie += initialisationsNeoPixel();
+  initilisation_reussie += initialisationUART();
+  initilisation_reussie += initialisationGPIO();
+
+  // I2C
+  Wire.begin(GPIO_I2C_SDA, GPIO_I2C_SCL);
+
+  delay(500);
+
   Serial.begin(115200);
   Serial.println("Booting");
 
@@ -46,13 +212,13 @@ void setup()
     ESP.restart();
   }
 #else
-  WiFi.disconnect(true);                                                             // disconnect form wifi to set new wifi connection
-  WiFi.mode(WIFI_STA);                                                               // init wifi mode
-  esp_wifi_sta_wpa2_ent_set_identity((uint8_t *)EAP_IDENTITY, strlen(EAP_IDENTITY)); // provide identity
-  esp_wifi_sta_wpa2_ent_set_username((uint8_t *)EAP_IDENTITY, strlen(EAP_IDENTITY)); // provide username --> identity and username is same
-  esp_wifi_sta_wpa2_ent_set_password((uint8_t *)EAP_PASSWORD, strlen(EAP_PASSWORD)); // provide password
+  WiFi.disconnect(true);
+  WiFi.mode(WIFI_STA);
+  esp_wifi_sta_wpa2_ent_set_identity((uint8_t *)EAP_IDENTITY, strlen(EAP_IDENTITY));
+  esp_wifi_sta_wpa2_ent_set_username((uint8_t *)EAP_IDENTITY, strlen(EAP_IDENTITY));
+  esp_wifi_sta_wpa2_ent_set_password((uint8_t *)EAP_PASSWORD, strlen(EAP_PASSWORD));
   esp_wifi_sta_wpa2_ent_enable();
-  WiFi.begin(ssid); // connect to wifi
+  WiFi.begin(ssid);
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(500);
@@ -109,18 +275,46 @@ void setup()
   ArduinoOTA.begin();
 
 #endif
+
+  
+  
+  digitalWrite(GPIO_MS1_G, LOW);
+  digitalWrite(GPIO_MS2_G, HIGH);
+  digitalWrite(GPIO_MS3_G, LOW);
+
+  digitalWrite(GPIO_MS1_D, LOW);
+  digitalWrite(GPIO_MS2_D, HIGH);
+  digitalWrite(GPIO_MS3_D, LOW);
+
+  Timer0_Cfg = timerBegin(0, 80, true); // timer incrémente toutes les 1us
+  
+  timerAttachInterrupt(Timer0_Cfg, &Timer0_ISR, true);
+  timerAlarmWrite(Timer0_Cfg, delai_timer_moteur_1, true);
+  timerAlarmEnable(Timer0_Cfg);
+
+
+  Timer3_Cfg = timerBegin(3, 80, true); // timer incrémente toutes les 1us
+  
+  timerAttachInterrupt(Timer3_Cfg, &Timer3_ISR, true);
+  timerAlarmWrite(Timer3_Cfg, delai_timer_moteur_2, true);
+  timerAlarmEnable(Timer3_Cfg);
+
 }
+
 
 void loop()
 {
-#ifdef WIFI_ACTIVE
-  ArduinoOTA.handle();
-#endif
+  pinMode(GPIO_ENABLE_MOTEURS, OUTPUT);
+  digitalWrite(GPIO_ENABLE_MOTEURS, LOW);
+  printf("Moteurs On\r\n");
 
-  timeout++;
 
-  if (timeout % 1000 == 0)
-  {
-    Serial.println("version 5");
-  }
+  delay(1000);
+
+
+
+ 
 }
+
+
+
